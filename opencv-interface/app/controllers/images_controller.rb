92e4497a44b8bb1330bb16c88405634221fccdf6
“felipelm” <felipelm123@gmail.com>
2016-09-09 13:36:05 -0300

    each user has his own set of pictures

--- opencv-interface/app/controllers/images_controller.rb
+++ opencv-interface/app/controllers/images_controller.rb
@@ -41,6 +41,7 @@ class ImagesController < ApplicationController
   # POST /images.json
   def create
     @image = Image.new(image_params)
+    @image.user_id = current_user.id
 
     respond_to do |format|
       if @image.save
